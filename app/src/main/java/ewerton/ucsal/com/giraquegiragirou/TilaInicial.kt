4d2e254e6bbe6839babbebbae7e9984b21a7f8aa
EwertonProg <e.luis0990@gmail.com>
2018-09-14 10:00:33 -0300

    Correções logicas

--- app/src/main/java/ewerton/ucsal/com/giraquegiragirou/TilaInicial.kt
+++ app/src/main/java/ewerton/ucsal/com/giraquegiragirou/TilaInicial.kt
@@ -8,44 +8,51 @@ import android.graphics.drawable.AnimationDrawable
 import android.widget.TextView
 
 
-class TilaInicial : AppCompatActivity() {
-    private val img = findViewById(R.id.imgDado) as ImageView;
-    private val animacao = img.background as AnimationDrawable
-    private val textPonto = findViewById(R.id.textPontos) as TextView;
-    private var pontos = 0;
+public  class TilaInicial : AppCompatActivity() {
+    private var img: ImageView? = null
+    private var animacao: AnimationDrawable? = null
+    private var textPonto: TextView? = null
+    private var pontos = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tila_inicial)
+         img = findViewById(R.id.imgDado)
+         img!!.setBackgroundResource(R.drawable.animacao)
+         animacao = img!!.background as AnimationDrawable
+         textPonto = findViewById(R.id.textPontos) as TextView
+         pontos = 0
     }
 
     fun Rolar (view: View) {
-        img.setBackgroundResource(R.drawable.animacao);
-        animacao.start();
+
+        animacao?.start()
     }
 
     fun Parar (view: View){
-        animacao.stop()
-        if (animacao.current.equals(R.drawable.dice1)){
-            pontos += 1;
-        }else if (animacao.current.equals(R.drawable.dice1)){
-            pontos += 2;
-        }else if (animacao.current.equals(R.drawable.dice1)){
-            pontos += 3;
-        }else if (animacao.current.equals(R.drawable.dice1)){
-            pontos += 4;
-        }else if (animacao.current.equals(R.drawable.dice1)){
-            pontos += 5;
-        }else{
-            pontos += 6;
-        }
+        if(animacao!!.isRunning) {
+            animacao?.stop()
+            if (animacao!!.current!!.equals(animacao!!.getFrame(0))) {
+                pontos += 1
+            } else if (animacao!!.current!!.equals(animacao!!.getFrame(1))) {
+                pontos += 2
+            } else if (animacao!!.current!!.equals(animacao!!.getFrame(2))) {
+                pontos += 3
+            } else if (animacao!!.current!!.equals(animacao!!.getFrame(3))) {
+                pontos += 4
+            } else if (animacao!!.current!!.equals(animacao!!.getFrame(4))) {
+                pontos += 5
+            } else {
+                pontos += 6
+            }
 
-        textPonto.setText(R.string.Pontos + pontos);
+            textPonto!!.setText("Pontos: " + pontos)
+        }
     }
 
     fun Reiniciar (view: View){
-        animacao.stop();
-        pontos = 0;
-        textPonto.setText(R.string.Pontos);
+        animacao!!.stop()
+        pontos = 0
+        textPonto!!.setText(R.string.Pontos)
     }
 }
 
